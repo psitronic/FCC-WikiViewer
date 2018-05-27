914317c82545ff7245e68ff540ea57bd499514e0
psitronic <spintronic@tuta.io>
2018-05-27 10:10:03 +0200

    Refactor the code and some style changes

--- assets/js/wikiviewer.js
+++ assets/js/wikiviewer.js
@@ -6,12 +6,14 @@ $("input[type='search']").on("keypress", function(event){
 
 		if(query !== ""){
 
-			var wikiURL = 'http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='  + query;
+			var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch='  + query;
 			
 			// clean an input area
 			$(this).val("");
 
-		    $.getJSON(wikiURL, updateSearchResults);
+	    $.getJSON(wikiURL)
+	        .done(updateSearchResults)
+	        .fail(errorSearch);
 	    }
 	}
 });
@@ -20,20 +22,30 @@ function updateSearchResults(data) {
 
 	var articles = data.query.search;
 
+
 	// if a list of articles already exists, delete it
 	if($("#articles")){
-		$("#articles").remove();	
+		$("#articles").remove();
 	};
 
 	// create a new ul list
 	$("#container").append("<ul id='articles'></ul>");
 
-	// loop through articles and add them to lis
-	articles.forEach(article => {
-		var title = article.title;
-		var snippet = article.snippet;
-		var link = "https://en.wikipedia.org/?curid=" + article.pageid;
+	if(articles.length === 0){
+		$("ul").append("<li>Nothing was found. Please try new search!</li>").hide().fadeIn(1000);
+	} else {
 
-		$("ul").append("<a href='" + link + "' target='#'>" + "<li>" + title + "<p>" + snippet + "</p>" +"</li>" + "</a>");
-	});
+		// loop through articles and add them to lis
+		articles.forEach(article => {
+			var title = article.title;
+			var snippet = article.snippet;
+			var link = "https://en.wikipedia.org/?curid=" + article.pageid;
+
+			$("ul").append("<a href='" + link + "' target='#'>" + "<li>" + title + "<p>" + snippet + "</p>" +"</li>" + "</a>").hide().fadeIn(1000);
+		});
+	};
+};
+
+function errorSearch() {
+	alert("Unable to retrieve data from Wikipedia");
 };
